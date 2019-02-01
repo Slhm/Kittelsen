c28b0c16e9453e9860dad8dd3e3b7e86b3f4f2e0
MartinSolheim <martyzno@gmail.com>
2019-02-01 01:38:47 +0100

    cozy quick fix

--- cmds/cozy.js
+++ cmds/cozy.js
@@ -11,23 +11,27 @@ module.exports.run = async (client, input, args, con) => {
     await con.query('SELECT * FROM cozy', (e, rows) => {
       let list  = "";
       rows.forEach((row, i) => {
-        list += i + ": " + "<" + row.link + ">" + "\n";
+        list += i+1 + ": " + "<" + row.link + ">" + "\n";
       });
       input.channel.send("items in db: \n" + list);
     })
   }else if(isOwner(input) && args[1] === "rm"){
     await con.query('DELETE FROM cozy WHERE id = ' + args[2], (e,rows) => {
       input.channel.send("removed item.");
+      con.query('SET @count = 0;');
+      con.query('UPDATE cozy SET cozy.id = @count := @count + 1;');
+      con.query('ALTER TABLE cozy AUTO_INCREMENT = 1');
     })
   } else {
     let r;
     await con.query('SELECT COUNT(*) AS count FROM cozy', (e, rows) => {
       r = Math.floor(Math.random() * rows[0].count) + 1;
+      console.log("r: " + r);
       //console.log("rows:  " + rows[0].count);
       let q = 'SELECT link FROM cozy WHERE id = ' + r;
-      con.query(q, (e, rows) => {
+      con.query(q, (e, row) => {
         //console.log("r: " + r + "\nlink: " + rows + ", link[0]: " + rows[0].link);
-        input.channel.send("cozy shit: " + rows[0].link);
+        input.channel.send("cozyyyyyyy: " + row[0].link);
       });
     });
   }
