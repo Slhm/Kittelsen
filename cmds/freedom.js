1b8dbd5960ca7f0b1e84520ef3b72404f78744a6
Slhm <martyzno@gmail.com>
2020-01-20 16:18:41 +0100

    feet/inches to meter now and vice versa added

--- cmds/freedom.js
+++ cmds/freedom.js
@@ -7,70 +7,86 @@ module.exports.run = async (client, input, args) => {
   args[1] = args[1].toLowerCase();
   args[2] = args[2] ? args[2].toLowerCase() : "";
   args[1] = args[1].replace(/\s*$/,"");
+  console.log("args[1]: " + args[1] + "\nargs[2]: " + args[2]);
 
-
+    // F to C
   if (checkUnit(args, ["f", "fahrenheit"])) {
     let F = toAmount(args[1]);
     let C = Math.round(((F - 32) / 1.8) * 10) / 10;
     input.channel.send(args[1] + " in non-freedomUnits is: " + C + "C");
-
+    // C to F
   } else if (checkUnit(args, ["c", "celsius"])) {
     let c = toAmount(args[1]);
     let f = Math.round((c * 1.8) + 32);
     input.channel.send(args[1] + " in freedoms is: " + f + "F");
 
+    // miles to km
   } else if (checkUnit(args, ["mph", "miles"])) {
     let mph = toAmount(args[1]);
     let kph = Math.round(mph * 1.609344 * 100) / 100;
     input.channel.send(args[1] + " in non-freedomUnits is: " + kph + " km(per hour if speed)");
 
+    // km to miles
   } else if (checkUnit(args, ["kph", "kmph", "km"])) {
     let kph = toAmount(args[1]);
     let mph = Math.round(kph * 0.6213712 * 100) / 100;
     input.channel.send(args[1] + " in freedomUnits is: " + mph + " miles(per hour if speed)");
 
+
+    // lbs to kg
   } else if (checkUnit(args, ["lbs", "pounds", "pound"])) {
     let lbs = toAmount(args[1]);
     let kg = Math.round(lbs * 0.4535924 * 100) / 100;
     input.channel.send(args[1] + " in non-freedomUnits is: " + kg + "kg");
 
+    // kg to lbs
   } else if (checkUnit(args, ["kg", "kilogram"])) {
     let kg = toAmount(args[1]);
     let lbs = Math.round(kg * 2.204623 * 100) / 100;
     input.channel.send(args[1] + " in freedomUnits is: " + lbs + "lbs");
 
+    // oz to liters
   } else if (checkUnit(args, ["oz", "ounce", "ounces"])) {
     let oz = toAmount(args[1]);
     let l = Math.round(oz * 0.0284131 * 100) / 100;
     input.channel.send(args[1] + " in non-freedomUnits is: " + l + " liters");
 
+    // liters to oz
   } else if (checkUnit(args, ["l", "liter", "liters"])) {
     let l = toAmount(args[1]);
     let oz = Math.round(l * 33.81413 * 100) / 100;
     input.channel.send(args[1] + " in freedomUnits is: " + oz + " oz");
   
+    // cm to inches
   }else if (checkUnit(args, ["cm"])) {
     let m = toAmount(args[1]);
     let inches = Math.round(m * 0.3937 * 100) / 100;
     input.channel.send(args[1] + " in freedomUnits is: " + inches + " inches");
   
+    // meters to feet
   }else if (checkUnit(args, ["m", "meter", "meters"])) {
     let m = toAmount(args[1]);
-    let feet = Math.round(m * 3.280840 * 100) / 100;
-    input.channel.send(args[1] + " in freedomUnits is: " + feet + " feet");
+    let feet = Math.round(m * 3.28084 * 100) / 100;
+    let feet_h = Math.floor(m * 3.28084);
+    let inches_h = Math.round((feet - feet_h) * 12 * 10) / 10;
+    console.log("feet: " + feet + "\nfeet_h: " + feet_h + "\ninches_h: " + inches_h);
+    input.channel.send(args[1] + " in freedomUnits is: " + feet + " feet" + "\nor if height: " + feet_h + "'" + inches_h + "\"");
 
-    //feet to meters
+    // feet to meters
   } else if (checkUnit(args, ["ft", "feet"])) {
     let feet = toAmount(args[1]);
     let m = Math.round(feet * 0.3048 * 100) / 100;
-    input.channel.send(args[1] + " in freedomUnits is: " + m + " meters");
+    input.channel.send(args[1] + " in  is: " + m + " meters");
   
-  } else if (checkUnit(args, ["'"]) && checkUnit(args, ["\""])) {
-    let feetInches = toAmount(args[1]);
+    // feet/inches to meters
+  } else if (args[1].includes("'") && args[1].includes("\"")) {
+    let feetInches = args[1];
     let feet = toAmount(feetInches.split("'")[0]);
     let inches = toAmount(feetInches.split("'")[1]);
-    let m0 = (feet * 0.3) + (inches * 0.0254);
+    let m0 = Math.round(((feet * 0.3048) + (inches * 0.0254)) * 100) / 100;
     input.channel.send(args[1] + " in freedomUnits is: " + m0 + " meters");
+
+    // Currency converter
   } else if (checkUnit(args, ["usd", "nok", "cad", "eur", "nzd"])) {
     //console.log("args[1]: " + args[1] + "args[2]: " + args[2]);
 	  await checkCurrencyList()
